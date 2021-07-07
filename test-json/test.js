fc9367f753b40656f03c15985be32ec67a8be47e
darccelio <darccelio@gmail.com>
2021-07-07 18:22:26 -0300

    inserção da tag <br> e condicionais para verificação se ha atributos id em pre label e pos label para não aparecer undefined

--- test-json/test.js
+++ test-json/test.js
@@ -1,102 +1,149 @@
-json_completo = `
+let json_completo = `
 {
-   "name":"faleconosco",
-   "method":"get",
-   "action":"enviado.html",
-   "autocomplete":true,
-   "items":[
-      {
-         "element":"input",
-         "type":"text",
-         "name":"nome",
-         "placeholder":"Digite seu nome",
-         "id":"nome"
-      },
-      {
-         "element":"input",
-         "type":"text",
-         "name":"cpf",
-         "placeholder":"Digite seu cpf",
-         "id":"cpf"
-      },
-      {
-         "element":"select",
-         "name":"gender",
-         "children":[
-            {
-               "element":"option",
-               "value":"F",
-               "innerhtml":"Feminino"
-            },
-            {
-               "element":"option",
-               "value":"M",
-               "innerhtml":"Masculino"
-            }
-         ]
-      },
-      {
-         "element":"input",
-         "type":"text",
-         "name":"endereco",
-         "placeholder":"Endereço completo: ",
-         "id":"endereco"
-      },
-      {
-         "element":"input",
-         "type":"text",
-         "name":"celular",
-         "placeholder":"Celular: (xx) xxxxx-xxxx",
-         "id":"celular"
-      },
-      {
-         "element":"input",
-         "type":"radio",
-         "value":"ADS",
-         "name":"curso",
-         "id":"ads",
-         "poslabel":"Análise e Desenvolvimento de Sistemas"
-      },
-      {
-         "element":"input",
-         "type":"radio",
-         "value":"EC",
-         "name":"curso",
-         "id":"ec",
-         "poslabel":"Engenharia de Computação"
-      },
-      {
-         "element":"input",
-         "type":"checkbox",
-         "value":"algoritmos",
-         "name":"disciplinas",
-         "id":"algoritmos",
-         "poslabel":"Algoritmos e Lógica de Programação"
-      },
-      {
-         "element":"input",
-         "type":"checkbox",
-         "value":"estruturas",
-         "name":"disciplinas",
-         "id":"estruturas",
-         "poslabel":"Estruturas de Dados"
-      },
-      {
-         "element":"input",
-         "type":"submit",
-         "value":"Enviar",
-         "name":"enviar"
-      }
-   ]
-}
+	"name":"complexo",
+	"method":"get",
+	"action":"consegui.html",
+	"autocomplete":true,
+	"items":[
+	   {
+		  "element":"input",
+		  "type":"text",
+		  "name":"cidade",
+		  "placeholder":"informe aqui o seu nome",
+		  "prelabel":"Nome:"
+	   },
+	   {
+		  "element":"input",
+		  "type":"text",
+		  "name":"cidade",
+		  "placeholder":"informe aqui a sua cidade",
+		  "prelabel":"Cidade:"
+	   },
+	   {
+		  "element":"select",
+		  "name":"idade",
+		  "children":[
+			 {
+				"element":"option",
+				"value":"less18",
+				"innerhtml":"Menos de 18 anos"
+			 },
+			 {
+				"element":"option",
+				"value":"18a59",
+				"innerhtml":"18 a 59"
+			 },
+			 {
+				"element":"option",
+				"value":"60a64",
+				"innerhtml":"60 a 64"
+			 },
+			 {
+				"element":"option",
+				"value":"65a69",
+				"innerhtml":"65 a 69"
+			 },
+			 {
+				"element":"option",
+				"value":"70a74",
+				"innerhtml":"70 a 74"
+			 },
+			 {
+				"element":"option",
+				"value":"75more",
+				"innerhtml":"75 ou mais"
+			 }
+		  ],
+		  "prelabel":"Qual a sua idade?"
+	   },
+	   {
+		  "element":"input",
+		  "type":"checkbox",
+		  "value":"saude",
+		  "name":"areaatuacao",
+		  "id":"cb-saude",
+		  "prelabel":"Qual a sua área de atuação?",
+		  "poslabel":"Saúde"
+	   },
+	   {
+		  "element":"input",
+		  "type":"checkbox",
+		  "value":"educacao",
+		  "name":"areaatuacao",
+		  "id":"cb-educacao",
+		  "poslabel":"Educação"
+	   },
+	   {
+		  "element":"input",
+		  "type":"checkbox",
+		  "value":"seguranca",
+		  "name":"areaatuacao",
+		  "id":"cb-seguranca",
+		  "poslabel":"Segurança"
+	   },
+	   {
+		  "element":"input",
+		  "type":"checkbox",
+		  "value":"outro",
+		  "name":"areaatuacao",
+		  "id":"cb-outro",
+		  "poslabel":"Outra"
+	   },
+	   {
+		  "element":"input",
+		  "type":"radio",
+		  "value":"sim",
+		  "name":"comorbidades",
+		  "id":"comorbidades-sim",
+		  "prelabel":"Você tem alguma comorbidade que agrave a COVID-19?",
+		  "poslabel":"Sim"
+	   },
+	   {
+		  "element":"input",
+		  "type":"radio",
+		  "value":"nao",
+		  "name":"comorbidades",
+		  "id":"comorbidades-nao",
+		  "poslabel":"Não"
+	   },
+	   {
+		  "element":"input",
+		  "type":"radio",
+		  "value":"sim",
+		  "name":"gestante",
+		  "id":"gestante-sim",
+		  "prelabel":"Você é gestante?",
+		  "poslabel":"Sim"
+	   },
+	   {
+		  "element":"input",
+		  "type":"radio",
+		  "value":"nao",
+		  "name":"gestante",
+		  "id":"gestante-nao",
+		  "poslabel":"Não"
+	   },
+	   {
+		  "element":"input",
+		  "type":"submit",
+		  "value":"Enviar"
+	   }
+	]
+ }
 `
 
-function parse_to_element(json, form) {
-
-    var elem = document.createElement(json['element']);
-
+function parse_to_element(json, form) {   
+    
+    let elem = document.createElement(json['element']);    
+    let br = document.createElement('br');
+    let padding = document.createAttribute('style');
+    
+    let div = document.createElement('div');
+    let tagClass = document.createAttribute('class');
+    
     if('type' in json){
         elem.type = json['type'];
+        padding.value = 'padding-left: 10px';
     }
     if('value' in json) {
         elem.value = json['value'];
@@ -109,33 +156,51 @@ function parse_to_element(json, form) {
     }
     if('id' in json) {
         elem.id = json['id'];
-    }
+    }       
+    
+
     if('prelabel' in json) {
-        var prelabel = document.createElement('label');
-        prelabel.htmlFor = json['id'];
-        prelabel.innerHTML = json['prelabel'];
+        let prelabel = document.createElement('label');
+        if('id' in json)
+            prelabel.htmlFor = json['id'];
+        if(elem.type == 'checkbox' || elem.type == 'radio'){
+            prelabel.innerHTML = json['prelabel']+'<br>';   
+        }
+        else
+            prelabel.innerHTML = json['prelabel'];  
+       
         form.appendChild(prelabel);
-    }
+
+    }   
+
     if('children' in json) {
-        for(var i = 0; i < json['children'].length; i++)
+        for(let i = 0; i < json['children'].length; i++){
             parse_to_element(json['children'][i], elem);
+        }
     }
     if('innerhtml' in json) {
         elem.innerHTML = json['innerhtml'];
     }
-
+    
     form.appendChild(elem);
 
+    
     if('poslabel' in json) {
-        var poslabel = document.createElement('label');
-        poslabel.htmlFor = json['id'];
+        let poslabel = document.createElement('label');
+        if('id' in json)
+            poslabel.htmlFor = json['id'];
+        
         poslabel.innerHTML = json['poslabel'];
-        form.appendChild(poslabel);
+        form.appendChild(poslabel);        
     }
+    
+    form.appendChild(br);
+    
 }
 
 function parse_to_form(json, elem) {
-    var form = document.createElement('form'); // <form> </form>
+    let form = document.createElement('form'); // <form> </form>
+    
     if('name' in json)
         form.name = json['name']; // <form name="nome"></form>
     if('method' in json)
@@ -151,11 +216,13 @@ function parse_to_form(json, elem) {
             parse_to_element(json['items'][i], form);
         }
     }
+    
     elem.appendChild(form);
+
 }
 
 function parse(elemOutputId) {
-    var elem = document.getElementById(elemOutputId);
-    var json = JSON.parse(json_completo);
+    let elem = document.getElementById(elemOutputId);
+    let json = JSON.parse(json_completo);
     parse_to_form(json, elem);
 }
